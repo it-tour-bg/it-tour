e1a024b346fa801ea9e6d52e82b60562ac2e6248
Radoslav Stankov <rstankov@gmail.com>
2013-06-21 16:36:42 +0200

    Respect the request domain

--- app/controllers/concerns/current_conference_methods.rb
+++ app/controllers/concerns/current_conference_methods.rb
@@ -9,10 +9,7 @@ module CurrentConferenceMethods
 
   def current_conference
     @current_conference ||= begin
-      domain = request.domain
-      domain = params[:domain] || 'varnaconf.com' # TODO remove when site goes public
-      domain = 'exampleconf.com' unless Rails.env.production?
-
+      domain = request.domain || params[:domain] || 'varnaconf.com' # TODO remove when site goes public
       Conference.find_for_domain(domain)
     end
   end
