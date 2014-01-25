968fb8bf0d1d4bae2218c929f57cc612698610bd
Dimitar Dimitrov <me@ddimitrov.name>
2014-01-25 17:25:35 +0200

    Remove the fog gem

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,1 @@
-
 CarrierWave.root = Rails.root.join(Rails.public_path)
-
-if Features.s3_uploads?
-  CarrierWave.configure do |config|
-    config.fog_directory   = ENV['S3_BUCKET']
-    config.fog_public      = true
-    config.fog_attributes  = {'Cache-Control' =>'max-age = 315576000'}
-    config.fog_credentials = {
-      provider:              'AWS',
-      aws_access_key_id:     ENV['S3_KEY'],
-      aws_secret_access_key: ENV['S3_SECRET']
-    }
-  end
-end
