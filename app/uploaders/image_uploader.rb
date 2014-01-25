1f92d2da3e3e4e35d199b7dfeadabdf7c49ebc58
Dimitar Dimitrov <me@ddimitrov.name>
2014-01-25 17:36:07 +0200

    Fix the image uploader's path
    It had "public" twice in the path. The first one comes from the
    initializer.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,7 +8,7 @@ module ImageUploader
   end
 
   def store_dir
-    "public/system/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "system/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   def filename
