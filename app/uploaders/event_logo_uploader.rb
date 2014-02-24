224c57c2394bc3151043f722bbb3514ba39f60f5
Radoslav Stankov <rstankov@gmail.com>
2014-02-24 22:52:11 +0100

    Add thumb size of logos

--- app/uploaders/event_logo_uploader.rb
+++ app/uploaders/event_logo_uploader.rb
@@ -4,5 +4,9 @@ class EventLogoUploader < CarrierWave::Uploader::Base
   version :favicon do
     process resize_to_fill: [50, 50]
   end
+
+  version :thumb do
+    process resize_to_fill: [88, 88]
+  end
 end
 
