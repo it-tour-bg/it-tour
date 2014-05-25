ff966ce421eed7027e61868213e713afaacf24fe
Radoslav Stankov <rstankov@gmail.com>
2014-05-25 15:47:05 +0300

    Add counters in the admin panel

--- app/helpers/title_helper.rb
+++ app/helpers/title_helper.rb
@@ -1,10 +1,15 @@
 module TitleHelper
-  def title(title)
+  def title(title, counter = nil)
     @_title = title
+    @_title_counter = counter
   end
 
   def title_value
     @_title
+  end
+
+  def title_header
+    "#{@_title}#{' (' + @_title_counter.to_s + ')' if @_title_counter.present?}"
   end
 
   def title_with_base(base_title)
