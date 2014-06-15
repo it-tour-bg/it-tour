8ca3e5f0be1979aa8fd3ba55e3ad8a6dfd7f24c2
Radoslav Stankov <rstankov@gmail.com>
2014-06-15 17:29:36 +0200

    Add rpec feature spec helper

---
+++ spec/spec_helper_features.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+require 'capybara/rspec'
+require 'capybara/rails'
+require 'capybara/session'
+require 'capybara/poltergeist'
+
+TourConf::Application.config.cache_classes = true
+
+RSpec.configure do |config|
+  config.use_transactional_fixtures = false
+
+  config.before :each do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.start
+  end
+
+  config.after :each do
+    DatabaseCleaner.clean
+  end
+end
+
+Capybara.configure do |config|
+  config.javascript_driver = :poltergeist
+  config.default_wait_time = 5
+end
+
