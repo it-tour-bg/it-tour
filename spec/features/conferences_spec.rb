88a06f6e6ea6cf787060808cafd988704225455d
Radoslav Stankov <rstankov@gmail.com>
2014-06-17 19:49:27 +0200

    Hide archive section when conference have only one event

---
+++ spec/features/conferences_spec.rb
@@ -0,0 +1,40 @@
+# encode: utf-8
+require 'spec_helper'
+
+describe "Conference" do
+  context "main conference" do
+
+  end
+
+  context "normal conference" do
+    let(:conference) { create :conference, domain: 'example.com' }
+
+    describe "Subscribe" do
+      it "is always displayed" do
+        create :event, conference: conference
+
+        visit root_path
+
+        expect(page).to have_content 'Записвания'
+      end
+    end
+
+    describe "Archive" do
+      it "isn't displayed when they aren't other event of current conference" do
+        create :event, conference: conference
+
+        visit root_path
+
+        expect(page).not_to have_content 'Архив'
+      end
+
+      it "is displayed when they aren't at least two events current conference" do
+        2.times { create :event, conference: conference }
+
+        visit root_path
+
+        expect(page).to have_content 'Архив'
+      end
+    end
+  end
+end
