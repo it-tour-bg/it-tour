10f491b89abd80e8df5164176d3d2934dc539092
Radoslav Stankov <rstankov@gmail.com>
2015-07-20 00:02:02 +0300

    Display multi track session in the frontend

--- spec/features/event_session_and_speakers_spec.rb
+++ spec/features/event_session_and_speakers_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper_features'
 
 feature "Event - session and speakers" do
   let(:event) { create :event, {conference: create(:conference, domain: 'example.com')}.merge(event_attributes) }
+  let(:event_attributes) { {speakers_announced: true, sessions_announced: true} }
 
   let!(:session) { create :session, event: event }
+
 
   context "sessions not announced" do
     let(:event_attributes) { {sessions_announced: false} }
@@ -76,6 +78,26 @@ feature "Event - session and speakers" do
       expect(page).to have_content speaker1.description
       expect(page).to have_content speaker2.name
       expect(page).to have_content speaker2.description
+    end
+  end
+
+  context 'multi-track sessions' do
+    it 'do not display track names if there is only one track' do
+      session_1 = create :session, event: event, track: 1
+
+      visit root_path
+
+      expect(page).not_to have_text 'Track 1'
+    end
+
+    it 'displays two tracks' do
+      session_1 = create :session, event: event, track: 1
+      session_2 = create :session, event: event, track: 2
+
+      visit root_path
+
+      expect(page).to have_text 'Track 1'
+      expect(page).to have_text 'Track 2'
     end
   end
 end
