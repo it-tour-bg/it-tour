551492391310a9b49ce439970f12749afea9e2ac
Petko Bordjukov <bordjukov@gmail.com>
2015-08-12 20:36:46 +0300

    Add CORS headers to the SchedulesController actions

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,4 +1,6 @@
 class SchedulesController < ApplicationController
+  before_action :set_access_control_headers
+
   def index
     event = current_conference.current_event
     redirect_to schedule_path(event.name, format: :json)
@@ -6,5 +8,13 @@ class SchedulesController < ApplicationController
 
   def show
     @event = current_conference.announced_event_named(params[:id])
+  end
+
+  private
+  def set_access_control_headers
+    if request.format.json?
+      response.headers['Access-Control-Allow-Origin'] = '*'
+      response.headers['Access-Control-Request-Method'] = '*'
+    end
   end
 end
